4e266ec19021864455f75d707626a247415f0e13
HRush <hrkoren@gmail.com>
2021-04-18 19:27:41 -0400

    continuing work on route coding

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -5,26 +5,43 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // get all products
 router.get('/', (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
+  try {
+    const prodData = await Product.findAll({
+      include: [{ model: Category, Tag }],
+    });
+    res.status(200).json(prodData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-// get one product
+// find a single product by its `id`
 router.get('/:id', (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+  try {
+    const prodData = await Product.findByPk(req.params.id, {
+      include: [{ model: Category, Tag }],
+    });
+    if (!prodData) {
+      res.status(404).json({ message: 'No product found with that id.' });
+      return;
+    }
+    res.status(200).json(prodData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // create new product
 router.post('/', (req, res) => {
-  /* req.body should look like this...
+  Product.createNew([
     {
-      product_name: "Basketball",
-      price: 200.00,
+      product_name: "Cardigan",
+      price: 50.00,
       stock: 3,
       tagIds: [1, 2, 3, 4]
     }
-  */
+  ])
+
   Product.create(req.body)
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
